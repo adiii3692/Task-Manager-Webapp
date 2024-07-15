a0799803dc3860af94e496805ede38ea830a9121
Aditya Nair <adinair0206@gmail.com>
2024-07-15 16:06:40 -0400

    Added render domain

--- server/routes/appRoutes.js
+++ server/routes/appRoutes.js
@@ -115,7 +115,8 @@ router.post("/login", async (request, response) => {
     //Create a jwt token and send it using cookies
     const jwToken = createToken(checkUsername[0]._id);
     response.cookie("jwt", jwToken, {
-      sameSite: false,
+      sameSite: 'None',
+      domain: '.onrender.com',
       httpOnly: false,
       maxAge: 60 * 1000 * 60,
 
